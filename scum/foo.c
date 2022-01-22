5b2c0965e6652f3347d4be475b209b451c84d06f
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-01-21 23:52:38 -0700

    Well fetch; I just called a Rust function from C, yo!

---
+++ scum/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "rusty.h"
+
+int main(int argc, char* argv[]) {
+  printf("Hello, this is C\n");
+  greet_generic();
+}
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub extern "C" fn greet_generic() {
+    println!("Greetings chap. Rust has been called.");
+}
