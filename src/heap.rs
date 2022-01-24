76950c18ca182f20fa549cdf4cf1dcd6212cb1eb
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2022-01-24 11:17:17 -0700

    Heap seems to be working

--- src/heap.rs
+++ src/heap.rs
@@ -9,9 +9,9 @@ pub struct Heap<T> where
 impl<T> Heap<T> where
     T: PartialOrd {
 
-    pub fn new(init: T) -> Heap<T> {
+    #[allow(unused_mut)]
+    pub fn new() -> Heap<T> {
         let mut the_heap = Vec::new();
-        the_heap.push(init);
 
         Heap {
             heap: the_heap
@@ -24,10 +24,35 @@ impl<T> Heap<T> where
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        None
+        if self.heap.len() == 0 {
+            None
+        }
+        else {
+            let ret = self.heap.swap_remove(0);
+            self.sift_down(0);
+            Some(ret)
+        }
     }
 
     fn sift_down(&mut self, sift: usize) {
+        let (chld_lix, chld_rix) = child_idxs(sift);
+        match (self.heap.get(chld_lix), self.heap.get(chld_rix)) {
+            (Some(a), Some(b)) => {
+                let (idx, smallest) = if a <= b { (chld_lix, a) } else { (chld_rix, b) };
+                if self.heap[sift] > *smallest {
+                    self.heap.swap(sift, idx);
+                    self.sift_down(idx)
+                }
+            },
+            (Some(a), None) => {
+                if self.heap[sift] > *a {
+                    self.heap.swap(chld_lix, sift);
+                    self.sift_down(chld_lix)
+                }
+            },
+            (None, None) => (),
+            (None, _) => panic!("Malformed heap! Missing left element!")
+        }
     }
 
     fn bubble_up(&mut self) {
@@ -35,16 +60,12 @@ impl<T> Heap<T> where
     }
 
     fn bubble_up_n(&mut self, bubble: usize) {
-        if bubble == 0 {
-            return;             // at top of heap; stop now
-        }
+        if let Some(parent) = parent_idx(bubble) {
+            if self.heap[bubble] < self.heap[parent] {
+                self.heap.swap(bubble, parent);
 
-        let parent = parent_idx(bubble).unwrap();
-
-        if self.heap[bubble] < self.heap[parent] {
-            self.heap.swap(bubble, parent);
-
-            self.bubble_up_n(parent);
+                self.bubble_up_n(parent);
+            }
         }
     }
 
@@ -60,7 +81,7 @@ fn parent_idx(a: usize) -> Option<usize> {
 }
 
 fn child_idxs(a: usize) -> (usize, usize) {
-    (2 * a, (2 * a) + 1)
+    ((2 * a) + 1, (2 * a) + 2)
 }
 
 #[cfg(test)]
@@ -80,12 +101,41 @@ mod tests {
     }
 
     #[test]
+    fn child_assumptions() {
+        assert_eq!(child_idxs(0), (1, 2));
+        assert_eq!(child_idxs(1), (3, 4));
+        assert_eq!(child_idxs(2), (5, 6));
+        assert_eq!(child_idxs(3), (7, 8));
+        assert_eq!(child_idxs(4), (9, 10));
+    }
+
+    #[test]
     fn new_and_insert() {
-        let mut foo = Heap::new(10);
+        let mut foo = Heap::new();
+        foo.push(10);
         foo.push(42);
         foo.push(5);
         foo.push(3);
         foo.push(4);
         assert_eq!(foo.heap, vec![3, 4, 10, 42, 5])
+    }
+
+    #[test]
+    fn pop_off() {
+        let mut foo = Heap::new();
+        foo.push(10);
+        foo.push(42);
+        foo.push(5);
+        foo.push(3);
+        foo.push(4);
+        assert_eq!(foo.heap, vec![3, 4, 10, 42, 5]);
+
+        assert_eq!(foo.pop(), Some(3));
+        assert_eq!(foo.pop(), Some(4));
+        assert_eq!(foo.pop(), Some(5));
+        assert_eq!(foo.pop(), Some(10));
+        assert_eq!(foo.pop(), Some(42));
+        assert_eq!(foo.pop(), None);
+        assert_eq!(foo.pop(), None);
     }
 }
